974a3275fdb0e721a91acafd5a9304fd1627f482
Marta Pleszynska <marta.pleszynska@mayden.co.uk>
2015-10-11 14:06:38 +0100

    added post

---
+++ _posts/2015-10-05.php
@@ -0,0 +1,25 @@
+<?php
+
+
+$postno = "2015-10-05";
+$myposttitle = "UI and UX and other stuff";
+$mypostauthor = "Marta Pleszynska";
+$mypostdate = "Oct 05, 2015";
+$mypostcontent = "
+<p class=\"mysposttitle\">UI - user interface</p>
+It is a set of commands and menus through which user interacts with an application.It is concentrated on how does it make the user feel and it aims to predict user's intentions.</p>
+<p class=\"mysposttitle\">UX - user experience</p>
+<p>It is concetrated on the way user interact with an application and user's journey within the program. User experience user's feelings towards using a product.</p>
+<img scr=\"/img/ux.png\">
+<br>
+<p class=\"mysposttitle\">Adaptive design</p>
+<p>With adaptive design there isn’t one layout that always changes. Instead, there are several distinct layouts for multiple screen sizes, and the layout used depends on the screen size used.<br>
+Adaptive is Less Flexible</p>
+<p class=\"myposttitle\">Responsive design(best approach)</p>
+<p>It provides the optimal viewing experience of a website, no matter what type of device the user is seeing it on.<br> Responsive Sites Load Faster</p>
+<p class=\"myposttitle\">Other stuff to know</p>
+<p>Viewport - a framed area on a display screen for viewing information</p>
+<p>Mobile first approach - tendency to design their products for mobile phones or devices before making correlate designs for traditional desktop and laptop computers. A desktop first approach is other way round.<br>
+Consider purpose of the product first before deciding to which approach to implement.</p>
+";
+?>
