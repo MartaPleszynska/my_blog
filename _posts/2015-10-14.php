a4d87004535f62e0b08307fd7a5c59ed7a5e8ecb
Marta Pleszynska <marta.pleszynska@mayden.co.uk>
2015-10-16 16:14:12 +0100

    added post

---
+++ _posts/2015-10-14.php
@@ -0,0 +1,70 @@
+<?php
+
+
+$postno = "2015-10-14";
+$myposttitle = "About AGILE and Scrum";
+$mypostauthor = "Marta Pleszynska";
+$mypostdate = "Oct 14, 2015";
+$mypostcontent = " ?>
+<p>Agile software development is a group of software development methods in which solutions evolve through collaboration between self-organizing, cross-functional teams. It promotes adaptive planning, evolutionary development, early delivery, continuous improvement, and encourages rapid and flexible response to change. <a href="https://en.wikipedia.org/wiki/Agile_software_development">(Wikipedia)</a></p>
+<img class = "centered halfsize" src="img/agile_manifesto.jpg" alt="agile manifesto">
+<h2 class="subposttitle">Agile Manifesto Principles:</h2>
+<ol class="list">
+<li>Our highest priority is to satisfy the customer through early and continuous delivery if valuable software.</li>
+<li>Welcome changing requirements, even late in development. Agile processes harness change for the customer's competitive advantage.</li>
+<li>Deliver working software frequently, from a couple of week to a couple of months, with a preference to the shorter timescale.</li>
+<li>Business people and developers must work together daily throughout the project.</li>
+<li>Build projects around motivated individuals. Give them the environment and support the need, and trust them to get the job done.</li>
+<li>The most efficient and effective method of conveying information to and within a development team is face-to-face conversation.</li>
+<li>Working software is the primary measure of progress.</li>
+<li>Agile processes promote sustainable development. The sponsors, developers, and users should be able to maintain a constant pace indefinitely.</li>
+<li>Continuous attention to technical exelence and good design enhances agility.</li>
+<li>Simplicity - the art of maximizing the amount of work not done - is essential.</li>
+<li>The best architectures, requirements, and designs emerge from self-orginizing teams.</li>
+<li>At regular intervals, the team reflects on how to become more effective, then tunes and adjusts its behaviour accordingly.</li>
+</ol>
+<img class="centered halfsize"" src="img/pocket_principles.jpg" alt="pocket agile principles">
+<p class="subposttiltle">What is <a href="https://en.wikipedia.org/wiki/Scrum_(software_development)">Scrum</a>?</p>
+<p>Scrum is a tool in helping to follow AGILE methodology but not the AGILE itself. It is software development methodology for managing product development with a strategy where a development team works as unit towards reaching a common goal.<br>
+Daily Scrum - is a 15 minutes stand up daily meeting, taken at the same time and place every day; it is facilitated by Scrum Master; each person says what they have done since last meeting, what they are planning to do until the next one and is there anything that in their way.<br>
+<h2 class="subposttitle">Scrum planning part II:</h2>
+<ul class="list">
+<li>How many stories can be done in one sprint?</li>
+<li>How many hours (or other units of measure) team has got in a sprint?</li>
+<li>Scrum Master cares how many hours are available per day and per sprint taking holidays, training and other thing into consideration?</li>
+<li>Unexperienced developers should double their estimation time until they confident they are good in estimation.</li>
+</ul>
+<h2 class="subposttitle">Scrum Meeting and Scrum Artefacts</h2>
+<img src="">
+<h2 class="subposttitle">Scrum Values: commitment, focus, openness, courage, respect.</h2>
+<h2 class="subposttitle">Roles:</h2>
+<ul class="list">
+<li>Product Owner - has a respect from the team because of the relationship with customer/user</li>
+<li>Team member -  is versatile, fully committed, has T-shaped skills</li>
+<li>Scrum Master - has positive influence on the team, drive the team forward</li>
+</ul>
+<h2 class="subposttitle">Guideline for Product backlog:</h2>
+<ul class="list">
+<li>decomposing the top 20% of the product backlog to items of 1/2 sprint length</li>
+<li>do not want to fill with one item the whole sprint</li>
+<li>next 20% of the product back log are items which are bit bigger, we can afford them to take up a length of 1-2 sprint</li>
+<li>rest 60% are items which takes up around 3-6 sprints, these items are at most risk of not being completed, including a lot of little things that are not cost effective to fix them</li>
+</ul>
+<h2 class="subposttitle">Definition of done - about quality not actual features</h2>
+<ul class="list">
+<li>Manually tested in EI8+, Chrome, Safari and Firefox 4+</li>
+<li>Code reviewed</li>
+<li>Meet coding standards + PSR</li>
+<li>Pull request made</li>
+<li>Write DocBlocks</li>
+</ul>
+<h2 class="subposttitle">A Story (problem) - spelling out what a problem is:</h2>
+<ul>
+<li>As a: (who?)...</li>
+<li>I want: (what?)...</li>
+<li>So that: (I can do what?)...</li>
+<li>Use "5Why" rule to take problem apart the story to get more details?</li>
+<ul>
+<?php
+";
+?>
